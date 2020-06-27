90aad1807c65b367c557e084d4e0b4662c8959b3
Robert Anderson <robert.anderson.fl@gmail.com>
2020-06-27 15:30:38 -0400

    **npm install required - Font Awesome is working, and tweaked the welcome page slightly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>FeatherMarks</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="/css/app.css" rel="stylesheet"></head>
         <style>
             html, body {
                 background-color: #fff;
@@ -80,19 +81,13 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="title m-b-md">Feather Marks <i class="fas fa-feather-alt"></i></div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Patchx/feathermarksweb">GitHub</a>
                 </div>
             </div>
         </div>
